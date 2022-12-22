8f77d44728e259c50598655fd29f0aabb763ce21
Sergeii Riabushenko <serhii.riabushenko@sombrainc.com>
2022-12-22 10:39:04 +0200

    Initial

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KubeDeployWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : Controller
+    {
+        public ValuesController()
+        {
+
+        }
+
+        public IActionResult Get()
+        {
+
+            return Ok("Hello, world");
+        }
+    }
+}
